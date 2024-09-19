50fa36b4859ce87c99e20e5a2d53434425333b5f
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-09-19 20:06:06 -0300

    tudo devidamente configura

--- auth-service/src/main/java/tech/engix/auth_service/mapper/UserMapper.java
+++ auth-service/src/main/java/tech/engix/auth_service/mapper/UserMapper.java
@@ -2,25 +2,17 @@ package tech.engix.auth_service.mapper;
 
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.NullValuePropertyMappingStrategy;
 import tech.engix.auth_service.dto.SignUpDto;
 import tech.engix.auth_service.dto.UserResponseDTO;
-import tech.engix.auth_service.dto.user.UserDto;
 import tech.engix.auth_service.dto.user.UserUpdateDTO;
 import tech.engix.auth_service.model.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
-    @Mapping(target = "id", ignore = true)  // Ignora a propriedade 'id'
-    @Mapping(target = "authProvider", ignore = true)
+
     User toEntityInsert(SignUpDto dto);
 
-    @Mapping(source = "dto.name", target = "name")
-    @Mapping(source = "dto.email", target = "email")
     UserResponseDTO toInsertDto(User dto);
 
-    @Mapping(source = "entity.name", target = "name")
-    @Mapping(source = "entity.email", target = "email")
     UserUpdateDTO toUpdate(User entity);
 }
