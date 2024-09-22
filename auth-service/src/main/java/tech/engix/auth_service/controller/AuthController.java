c5c266fccca69e55897954c6725e070a9024e26b
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-09-22 18:47:58 -0300

    aproveitar q esta tudo funcionando

--- auth-service/src/main/java/tech/engix/auth_service/controller/AuthController.java
+++ auth-service/src/main/java/tech/engix/auth_service/controller/AuthController.java
@@ -1,8 +1,6 @@
 package tech.engix.auth_service.controller;
 
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,15 +11,11 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import tech.engix.auth_service.dto.LoginDto;
-import tech.engix.auth_service.dto.LoginResponseDTO;
-import tech.engix.auth_service.dto.SignUpDto;
-import tech.engix.auth_service.dto.UserResponseDTO;
+import org.springframework.web.bind.annotation.*;
+import tech.engix.auth_service.dto.*;
 import tech.engix.auth_service.security.jwt.JwtUtils;
+import tech.engix.auth_service.security.jwt.service.RefreshTokenService;
+import tech.engix.auth_service.security.services.CustomUserDetailsService;
 import tech.engix.auth_service.service.AuthService;
 
 @Slf4j
@@ -36,6 +30,11 @@ public class AuthController {
 
     private final JwtUtils jwtUtils;
 
+    private final RefreshTokenService refreshTokenService;
+
+
+    private final CustomUserDetailsService customUserDetailsService;
+
     @PostMapping("/signup")
     public ResponseEntity<UserResponseDTO> registerUser(@Valid @RequestBody SignUpDto dto) {
         UserResponseDTO sign = service.registerUser(dto);
@@ -43,30 +42,44 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<LoginResponseDTO> authenticateUser(@RequestBody LoginDto loginDto, HttpServletResponse response) {
-        Authentication authentication = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(loginDto.username(), loginDto.password()));
-        if (authentication == null) {
-            return ResponseEntity.badRequest().build();
+    public ResponseEntity<Object> authenticateUser(@RequestBody LoginDto loginDto) {
+        try {
+            Authentication authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginDto.username(), loginDto.password()));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            String accessToken = jwtUtils.generateJwtToken(authentication);
+
+            String refreshToken = jwtUtils.generateRefreshToken(authentication);
+
+            refreshTokenService.saveRefreshToken(loginDto.username(), refreshToken);
+
+            return ResponseEntity.ok().body(new LoginResponseDTO(accessToken, refreshToken));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Access denied: " + e.getMessage());
         }
-        SecurityContextHolder.getContext().setAuthentication(authentication);
-        String jwt = jwtUtils.generateJwtToken(authentication);
-        return ResponseEntity.ok().body(new LoginResponseDTO(jwt));
     }
 
-    @PostMapping("/oauth2/login/success")
-    public ResponseEntity<?> oauth2LoginSuccess(HttpServletRequest request) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    @PostMapping("/refresh")
+    public ResponseEntity<?> refresh(@RequestBody TokenRefreshRequest request) {
+        String refreshToken = request.refreshToken();
+        String username = jwtUtils.getUserNameFromJwtToken(refreshToken);
 
-        if (authentication == null) {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Unauthorized");
+        String storedRefreshToken = refreshTokenService.getRefreshToken(username);
+
+        if (storedRefreshToken == null || !storedRefreshToken.equals(refreshToken) || !jwtUtils.validateJwtToken(refreshToken)) {
+            return ResponseEntity.status(403).body("Invalid Refresh Token");
         }
 
-        String jwtToken = jwtUtils.generateJwtToken(authentication);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String newAccessToken = jwtUtils.generateJwtToken(authentication);
+        String newRefreshToken = jwtUtils.generateRefreshToken(authentication); // Gere um novo refresh token
 
-        log.info("Generated JWT Token for OAuth2 login: {}", jwtToken);
+        // Salve o novo refresh token no Redis
+        refreshTokenService.saveRefreshToken(username, newRefreshToken);
 
-        return ResponseEntity.ok(new LoginResponseDTO(jwtToken));
+        return ResponseEntity.ok(new TokenRefreshResponse(newAccessToken, newRefreshToken)); // Retorne o novo refresh token também
     }
 
 
