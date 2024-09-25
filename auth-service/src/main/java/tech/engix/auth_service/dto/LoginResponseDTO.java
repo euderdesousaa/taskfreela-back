f7113a68433b97f00575985147fa394047d6ff94
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-09-25 11:56:06 -0300

    resgatar byname

--- auth-service/src/main/java/tech/engix/auth_service/dto/LoginResponseDTO.java
+++ auth-service/src/main/java/tech/engix/auth_service/dto/LoginResponseDTO.java
@@ -4,5 +4,6 @@ import lombok.Builder;
 
 @Builder
 public record LoginResponseDTO(String jwt,
-                               String refreshToken) {
+                               String refreshToken,
+                               String name) {
 }
