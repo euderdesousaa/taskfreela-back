aa28e03f65eda9f8feed8ab3c9886df35d2610e7
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-09-25 19:22:18 -0300

    resgatar byname

--- auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
+++ auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
@@ -3,6 +3,7 @@ package tech.engix.auth_service.config;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
@@ -13,6 +14,9 @@ public class AppProperties {
     private final Auth auth = new Auth();
     private final OAuth2 oAuth2 = new OAuth2();
 
+    @Value(value = "${tech.engix.url}")
+    private static String url;
+
     @Getter
     @Setter
     public static class Auth {
@@ -22,7 +26,7 @@ public class AppProperties {
 
     @Getter
     public static final class OAuth2 {
-        private List<String> authorizedRedirectUris = List.of("http://localhost:3000/oauth2/redirect");
+        private List<String> authorizedRedirectUris = List.of(url +"/oauth2/redirect");
 
         public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
             this.authorizedRedirectUris = authorizedRedirectUris;
