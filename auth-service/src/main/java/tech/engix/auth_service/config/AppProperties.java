13e332dbcedda5e72ef4dbcebca74a7d1222e14c
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-09-26 18:15:13 -0300

    teste

--- auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
+++ auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
@@ -3,6 +3,7 @@ package tech.engix.auth_service.config;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.ArrayList;
@@ -11,6 +12,8 @@ import java.util.List;
 @Data
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
+    @Value(value = "${app.oauth2.authorizedRedirectUris}")
+    private List<String> authorizedRedirectUris = new ArrayList<>();
 
     private final Auth auth = new Auth();
     private final OAuth2 oAuth2 = new OAuth2();
@@ -23,7 +26,7 @@ public class AppProperties {
 
     @Getter
     public static final class OAuth2 {
-        private List<String> authorizedRedirectUris = new ArrayList<>();
+        private List<String> authorizedRedirectUris = List.of("https://taskfreela.vercel.app/oauth2/redirect");
 
         public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
             this.authorizedRedirectUris = authorizedRedirectUris;
