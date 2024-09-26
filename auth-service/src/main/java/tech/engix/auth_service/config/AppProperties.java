14d3c2e8549ef16a8c2fa287b8d267b2b8050827
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-09-26 18:28:56 -0300

    teste

--- auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
+++ auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
@@ -10,7 +10,6 @@ import java.util.List;
 @Data
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
-
     private final Auth auth = new Auth();
     private final OAuth2 oAuth2 = new OAuth2();
     @Getter
@@ -22,7 +21,7 @@ public class AppProperties {
 
     @Getter
     public static final class OAuth2 {
-        private List<String> authorizedRedirectUris = List.of("https://taskfreela.vercel.app/oauth2/redirect");
+        private List<String> authorizedRedirectUris = List.of("https://taskfreela.vercel.app/api/oauth2/redirect");
 
         public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
             this.authorizedRedirectUris = authorizedRedirectUris;
