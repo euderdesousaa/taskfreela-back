89f3e4e9dc457bd7a6666f5b1a9a212b314d0eb0
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-09-26 18:18:09 -0300

    teste

--- auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
+++ auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
@@ -3,17 +3,13 @@ package tech.engix.auth_service.config;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
-    @Value(value = "${app.oauth2.authorizedRedirectUris}")
-    private List<String> authorizedRedirectUris = new ArrayList<>();
 
     private final Auth auth = new Auth();
     private final OAuth2 oAuth2 = new OAuth2();
