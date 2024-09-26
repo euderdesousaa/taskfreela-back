735affde47c4ad41c308ae24d2584928df0bc758
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-09-26 17:33:58 -0300

    alterei URL

--- auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
+++ auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
@@ -5,11 +5,13 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
 @ConfigurationProperties(prefix = "app")
 public class AppProperties {
+
     private final Auth auth = new Auth();
     private final OAuth2 oAuth2 = new OAuth2();
     @Getter
@@ -21,7 +23,7 @@ public class AppProperties {
 
     @Getter
     public static final class OAuth2 {
-        private List<String> authorizedRedirectUris = List.of("http://localhost:3000/oauth2/api/oauth2/google/redirect");
+        private List<String> authorizedRedirectUris = new ArrayList<>();
 
         public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
             this.authorizedRedirectUris = authorizedRedirectUris;
