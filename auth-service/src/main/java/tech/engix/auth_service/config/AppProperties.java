d852b71c803affa58ff17220d96be0d15c8a1a18
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-09-26 21:03:45 -0300

    testes

--- auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
+++ auth-service/src/main/java/tech/engix/auth_service/config/AppProperties.java
@@ -21,7 +21,7 @@ public class AppProperties {
 
     @Getter
     public static final class OAuth2 {
-        private List<String> authorizedRedirectUris = List.of("https://www.taskfreela.com/api/oauth2/google/redirect");
+        private List<String> authorizedRedirectUris = List.of("https://www.taskfreela.com/api/oauth2/redirect");
 
         public OAuth2 authorizedRedirectUris(List<String> authorizedRedirectUris) {
             this.authorizedRedirectUris = authorizedRedirectUris;
