746fb9475b659c3a591ec36b09bb363bc6496f7e
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-10 14:29:38 -0300

    DTO

---
+++ project-service/src/main/java/tech/engix/project_service/dto/ProjectRequest.java
@@ -0,0 +1,15 @@
+package tech.engix.project_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+public record ProjectRequest(
+        String name,
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        Date createTime,
+        @JsonFormat(pattern = "dd/MM/yyyy")
+        Date finishTime,
+        Double price
+) {
+}
