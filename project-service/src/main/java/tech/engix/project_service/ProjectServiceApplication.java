4a617ada751b5aab5c40b1f58ffd25b6dbd4c14c
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-10 14:09:43 -0300

    project

---
+++ project-service/src/main/java/tech/engix/project_service/ProjectServiceApplication.java
@@ -0,0 +1,15 @@
+package tech.engix.project_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+
+public class ProjectServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectServiceApplication.class, args);
+	}
+
+}
