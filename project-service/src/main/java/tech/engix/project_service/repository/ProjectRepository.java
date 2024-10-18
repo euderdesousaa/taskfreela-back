a15f8c1cc60cf12c7c7abb79ec2bf3fc1def09d3
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-18 11:58:58 -0300

    new fields

--- project-service/src/main/java/tech/engix/project_service/repository/ProjectRepository.java
+++ project-service/src/main/java/tech/engix/project_service/repository/ProjectRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import tech.engix.project_service.model.Project;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+
+    List<Project> findByUserId(Long userId);
+
+    boolean existsByIdAndUserId(Long id, Long userId);
 }
