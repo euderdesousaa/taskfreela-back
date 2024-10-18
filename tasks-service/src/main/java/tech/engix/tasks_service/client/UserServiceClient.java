d77682ded36a1469439ea575a1745bf3b6a5871d
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-18 12:09:15 -0300

    UserService feign client

---
+++ tasks-service/src/main/java/tech/engix/tasks_service/client/UserServiceClient.java
@@ -0,0 +1,13 @@
+package tech.engix.tasks_service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tech.engix.tasks_service.dto.UserResponse;
+
+@FeignClient(url = "localhost:8081", name = "auth-service")
+public interface UserServiceClient {
+
+    @GetMapping("/api/v1/accounts/by-email")
+    UserResponse getUserByEmail(@RequestParam("email") String email);
+}
