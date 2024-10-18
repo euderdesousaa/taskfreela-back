af55dfe4148951447ee5f509f4c480f4feacfc71
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-18 11:58:17 -0300

    Mapper with toUpdate, updateEntity and toProjectResposne

--- project-service/src/main/java/tech/engix/project_service/mapper/ProjectMapper.java
+++ project-service/src/main/java/tech/engix/project_service/mapper/ProjectMapper.java
@@ -2,11 +2,20 @@ package tech.engix.project_service.mapper;
 
 import org.mapstruct.Mapper;
 import tech.engix.project_service.dto.ProjectRequest;
+import tech.engix.project_service.dto.ProjectResponse;
+import tech.engix.project_service.dto.ProjectUpdateRequest;
 import tech.engix.project_service.model.Project;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProjectMapper {
 
     Project toEntity(ProjectRequest projectRequest);
     ProjectRequest toProjectRequest(Project project);
+
+    ProjectUpdateRequest toUpdate(Project save);
+    Project toUpdateEntity(ProjectUpdateRequest project);
+
+    List<ProjectResponse> toProjectResponse(List<Project> projects);
 }
