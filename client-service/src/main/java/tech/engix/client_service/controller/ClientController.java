3e8c5b6ac4e0552939f4a6b5f179476f779d91d8
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-18 12:19:41 -0300

    Controller

---
+++ client-service/src/main/java/tech/engix/client_service/controller/ClientController.java
@@ -0,0 +1,72 @@
+package tech.engix.client_service.controller;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import tech.engix.client_service.dto.ClientRequest;
+import tech.engix.client_service.dto.ClientUpdateRequest;
+import tech.engix.client_service.model.Client;
+import tech.engix.client_service.service.ClientService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/client")
+@RequiredArgsConstructor
+public class ClientController {
+
+    private final ClientService service;
+
+    @GetMapping()
+    public ResponseEntity<List<Client>> listAllTasks(
+            HttpServletRequest request) {
+        String jwtToken = getJwtFromCookies(request);
+
+        if (jwtToken == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        return ResponseEntity.ok().body(service.listAll( jwtToken));
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<ClientRequest> createClient(@RequestBody ClientRequest dto, HttpServletRequest request) {
+        String jwtToken = getJwtFromCookies(request);
+
+        if (jwtToken == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+
+        ClientRequest createdClient = service.createAClient(dto, jwtToken);
+        return ResponseEntity.ok(createdClient);
+    }
+
+    @PutMapping("/edit/{id}")
+    public ResponseEntity<ClientUpdateRequest> edit(@PathVariable Long id,
+                                                    @RequestBody ClientUpdateRequest client,
+                                                    HttpServletRequest request) {
+        String jwtToken = getJwtFromCookies(request);
+        service.editClient(client, id, jwtToken);
+        return ResponseEntity.ok().body(client);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long id, HttpServletRequest request) {
+        String jwtToken = getJwtFromCookies(request);
+        service.deleteClient(id, jwtToken);
+    }
+
+    private String getJwtFromCookies(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if ("accessToken".equals(cookie.getName())) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return null;
+    }
+}
