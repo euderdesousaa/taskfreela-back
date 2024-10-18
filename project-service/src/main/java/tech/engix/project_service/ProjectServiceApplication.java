fd32d91e63f8463e9e770ca126c6f9f155aebd3b
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-18 11:59:56 -0300

    FeignClient annotation

--- project-service/src/main/java/tech/engix/project_service/ProjectServiceApplication.java
+++ project-service/src/main/java/tech/engix/project_service/ProjectServiceApplication.java
@@ -2,12 +2,13 @@ package tech.engix.project_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ProjectServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectServiceApplication.class, args);
 	}
-
 }
