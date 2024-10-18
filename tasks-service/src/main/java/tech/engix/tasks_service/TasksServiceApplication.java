4641d1da58a7027f90b44a29e8cddcd1125ad830
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-18 12:09:30 -0300

    FeignClient annotation

--- tasks-service/src/main/java/tech/engix/tasks_service/TasksServiceApplication.java
+++ tasks-service/src/main/java/tech/engix/tasks_service/TasksServiceApplication.java
@@ -2,8 +2,11 @@ package tech.engix.tasks_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@EnableFeignClients
 public class TasksServiceApplication {
 
 	public static void main(String[] args) {
