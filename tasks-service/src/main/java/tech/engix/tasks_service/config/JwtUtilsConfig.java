421ec456f02e67c7d8d504e3ab032a1bf9bfa78d
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-18 12:09:02 -0300

    JwtUtils

---
+++ tasks-service/src/main/java/tech/engix/tasks_service/config/JwtUtilsConfig.java
@@ -0,0 +1,18 @@
+package tech.engix.tasks_service.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import tech.engix.jwtutils.service.JwtUtils;
+
+@Configuration
+public class JwtUtilsConfig {
+
+    @Value(value = "${tech.engix.jwtSecret}")
+    private String secret;
+
+    @Bean
+    public JwtUtils jwtUtils() {
+        return new JwtUtils(secret);
+    }
+}
