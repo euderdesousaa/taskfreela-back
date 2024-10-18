b700307d607603eed2acded0e969365d17bf2682
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-18 12:20:08 -0300

    test

---
+++ client-service/src/test/java/tech/engix/client_service/ClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.engix.client_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
