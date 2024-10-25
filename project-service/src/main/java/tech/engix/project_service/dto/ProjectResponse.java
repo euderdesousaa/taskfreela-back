b181eadcdf05e823a0fe558c39851f649c9b46a3
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-25 11:19:58 -0300

    Responses

--- project-service/src/main/java/tech/engix/project_service/dto/ProjectResponse.java
+++ project-service/src/main/java/tech/engix/project_service/dto/ProjectResponse.java
@@ -1,16 +1,28 @@
 package tech.engix.project_service.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
-public record ProjectResponse(
-        String name,
-        @JsonFormat(pattern = "dd/MM/yyyy")
-        Date createTime,
-        @JsonFormat(pattern = "dd/MM/yyyy")
-        Date finishTime,
-        Double budget,
-        String team
-) {
+@Data
+@NoArgsConstructor
+public class ProjectResponse {
+    private Long id;
+
+    private String name;
+
+    private String clientName;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date createTime;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private Date finishTime;
+
+    private Double budget;
+
+    private String team;
+
 }
