673148e2d52f4360fd98e684e0b8d005a21cd5f3
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-25 11:21:25 -0300

    Revemoing generics exceptions and filtering by User

--- project-service/src/main/java/tech/engix/project_service/service/ProjectService.java
+++ project-service/src/main/java/tech/engix/project_service/service/ProjectService.java
@@ -1,19 +1,23 @@
 package tech.engix.project_service.service;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tech.engix.jwtutils.service.JwtUtils;
+import tech.engix.project_service.client.ClientServiceClient;
 import tech.engix.project_service.client.UserServiceClient;
-import tech.engix.project_service.dto.ProjectRequest;
-import tech.engix.project_service.dto.ProjectResponse;
-import tech.engix.project_service.dto.ProjectUpdateRequest;
-import tech.engix.project_service.dto.UserResponse;
+import tech.engix.project_service.dto.*;
 import tech.engix.project_service.mapper.ProjectMapper;
 import tech.engix.project_service.model.Project;
 import tech.engix.project_service.repository.ProjectRepository;
+import tech.engix.project_service.service.exception.exceptions.AccessDeniedException;
+import tech.engix.project_service.service.exception.exceptions.ClientNotFound;
+import tech.engix.project_service.service.exception.exceptions.ProjectNotFoundException;
 
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +29,7 @@ public class ProjectService {
     private final ProjectMapper mapper;
     private final JwtUtils jwtUtils;
     private final UserServiceClient userServiceClient;
+    private final ClientServiceClient clientServiceClient;
 
     public List<ProjectResponse> listAll(String jwtToken) {
         String email = jwtUtils.getUserNameFromJwtToken(jwtToken);
@@ -32,24 +37,45 @@ public class ProjectService {
         UserResponse user = userServiceClient.getUserByEmail(email);
 
         if (user != null) {
-            return mapper.toProjectResponse(repository.findByUserId(user.id()));
+            List<Project> projects = repository.findByUserId(user.id());
+
+            return projects.stream()
+                    .sorted(Comparator.comparing(Project::getId))
+                    .map(project -> {
+                        ClientResponse client = clientServiceClient.getClientName(project.getClientId());
+
+                        ProjectResponse projectResponse = mapper.toProjectResponse(project);
+
+                        projectResponse.setClientName(client.name());
+
+                        return projectResponse;
+                    })
+                    .toList();
         }
 
         return Collections.emptyList();
     }
 
-    public ProjectRequest createProject(ProjectRequest dto, String jwtToken) {
+    public Optional<ProjectClientResponse> getProjectId(Long id) {
+        Project findById = repository.findById(id).orElseThrow(EntityNotFoundException::new);
+        return Optional.ofNullable(mapper.toProjectClientResponse(findById));
+    }
+
+    @Transactional
+    public ProjectRequest createProject(ProjectRequest dto, String jwtToken, Long id) {
         String email = jwtUtils.getUserNameFromJwtToken(jwtToken);
         UserResponse user = userServiceClient.getUserByEmail(email);
+        ClientResponse client = clientServiceClient.getClientName(id);
 
         Project project = mapper.toEntity(dto);
 
         project.setUserId(user.id());
+        project.setClientId(client.id());
         return mapper.toProjectRequest(repository.save(project));
     }
 
+    @Transactional
     public ProjectUpdateRequest editProject(ProjectUpdateRequest update, Long id, String jwtToken) {
-
         String email = jwtUtils.getUserNameFromJwtToken(jwtToken);
         UserResponse user = userServiceClient.getUserByEmail(email);
 
@@ -59,16 +85,18 @@ public class ProjectService {
             Project project = projectOptional.get();
 
             if (project.getUserId().equals(user.id())) {
-                Project toMapper = mapper.toUpdateEntity(update);
-                return mapper.toUpdate(repository.save(toMapper));
+                mapper.toUpdateEntity(update, project);
+
+                return mapper.toUpdate(repository.save(project));
             } else {
-                throw new RuntimeException("Unauthorized: You do not have permission to edit this tasks.");
+                throw new AccessDeniedException("Unauthorized: You do not have permission to edit this project.");
             }
         } else {
-            throw new RuntimeException("Client not found with id " + id);
+            throw new ProjectNotFoundException("Project not found with id " + id);
         }
     }
 
+    @Transactional
     public void deleteProject(Long id, String jwtToken) {
         String email = jwtUtils.getUserNameFromJwtToken(jwtToken);
         UserResponse user = userServiceClient.getUserByEmail(email);
@@ -76,7 +104,7 @@ public class ProjectService {
         if (repository.existsByIdAndUserId(id, user.id())) {
             repository.deleteById(id);
         } else {
-            throw new RuntimeException("Client not found or user not authorized to delete this client.");
+            throw new ClientNotFound("Client not found or user not authorized to delete this client.");
         }
     }
 
