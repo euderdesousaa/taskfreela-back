78807911004f95d40f05e4db9d70fd6bdb0c9bb7
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-25 11:17:57 -0300

    toClientResponse

--- client-service/src/main/java/tech/engix/client_service/mapper/ClientMapper.java
+++ client-service/src/main/java/tech/engix/client_service/mapper/ClientMapper.java
@@ -2,8 +2,9 @@ package tech.engix.client_service.mapper;
 
 import org.mapstruct.Mapper;
 import tech.engix.client_service.dto.ClientRequest;
-import tech.engix.client_service.model.Client;
+import tech.engix.client_service.dto.ClientResponse;
 import tech.engix.client_service.dto.ClientUpdateRequest;
+import tech.engix.client_service.model.Client;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
@@ -12,4 +13,6 @@ public interface ClientMapper {
     ClientRequest toProjectRequest(Client client);
 
     ClientUpdateRequest toUpdate(Client save);
+
+    ClientResponse toClientResponse(Client client);
 }
