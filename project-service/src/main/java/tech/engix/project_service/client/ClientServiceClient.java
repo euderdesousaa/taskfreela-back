d16e51e9a7276098a13db91cb1adda199cd63bff
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-25 11:19:20 -0300

    Import client-service feignClient

---
+++ project-service/src/main/java/tech/engix/project_service/client/ClientServiceClient.java
@@ -0,0 +1,13 @@
+package tech.engix.project_service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tech.engix.project_service.dto.ClientResponse;
+
+@FeignClient(url = "localhost:8084", name = "client-service")
+public interface ClientServiceClient {
+
+    @GetMapping("api/v1/client/name")
+    ClientResponse getClientName(@RequestParam("id") Long id);
+}
