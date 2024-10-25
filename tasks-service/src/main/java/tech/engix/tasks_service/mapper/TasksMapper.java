d6901f87eddafbe221f249e2cde73309d8200736
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-25 11:23:34 -0300

    MapStruct annotation

--- tasks-service/src/main/java/tech/engix/tasks_service/mapper/TasksMapper.java
+++ tasks-service/src/main/java/tech/engix/tasks_service/mapper/TasksMapper.java
@@ -1,6 +1,8 @@
 package tech.engix.tasks_service.mapper;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import tech.engix.tasks_service.dto.RequestTasks;
 import tech.engix.tasks_service.dto.TasksUpdateRequest;
 import tech.engix.tasks_service.model.Tasks;
@@ -11,5 +13,6 @@ public interface TasksMapper {
     RequestTasks toEntity(Tasks tasks);
     Tasks toDto(RequestTasks tasks);
 
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
     TasksUpdateRequest toUpdate(Tasks save);
 }
