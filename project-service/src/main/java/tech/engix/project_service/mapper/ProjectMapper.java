a6e2c4ec5bbaff6d16e68f2c6d290d61a4695228
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-25 11:20:24 -0300

    @Mapping

--- project-service/src/main/java/tech/engix/project_service/mapper/ProjectMapper.java
+++ project-service/src/main/java/tech/engix/project_service/mapper/ProjectMapper.java
@@ -1,12 +1,11 @@
 package tech.engix.project_service.mapper;
 
-import org.mapstruct.Mapper;
+import org.mapstruct.*;
+import tech.engix.project_service.dto.ProjectClientResponse;
 import tech.engix.project_service.dto.ProjectRequest;
 import tech.engix.project_service.dto.ProjectResponse;
 import tech.engix.project_service.dto.ProjectUpdateRequest;
 import tech.engix.project_service.model.Project;
-
-import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface ProjectMapper {
@@ -14,8 +13,18 @@ public interface ProjectMapper {
     Project toEntity(ProjectRequest projectRequest);
     ProjectRequest toProjectRequest(Project project);
 
-    ProjectUpdateRequest toUpdate(Project save);
-    Project toUpdateEntity(ProjectUpdateRequest project);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    ProjectUpdateRequest toUpdate(Project project); // Corrigido para aceitar o projeto atual
 
-    List<ProjectResponse> toProjectResponse(List<Project> projects);
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "clientId", ignore = true) // Ignorando campos desnecessários
+    @Mapping(target = "userId", ignore = true)
+    @Mapping(target = "id", ignore = true)
+    void toUpdateEntity(ProjectUpdateRequest projectUpdateRequest, @MappingTarget Project project); // Agora aceitando um objeto existente
+
+    @Mapping(target = "clientName", ignore = true) // Corrigido para o mapeamento de resposta
+    ProjectResponse toProjectResponse(Project project);
+
+    ProjectClientResponse toProjectClientResponse(Project project);
+
 }
