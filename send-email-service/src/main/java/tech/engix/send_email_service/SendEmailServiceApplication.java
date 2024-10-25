cc029e1647dcd3c0c5b7cbdb00c5d84e43e9139e
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-25 11:04:49 -0300

    Send email service, i decided remove Send Email responsibility from Auth Service

---
+++ send-email-service/src/main/java/tech/engix/send_email_service/SendEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.engix.send_email_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendEmailServiceApplication.class, args);
+	}
+
+}
---
+++ send-email-service/src/test/java/tech/engix/send_email_service/SendEmailServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package tech.engix.send_email_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendEmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// comment explaining why the method is empty
+	}
+
+}
