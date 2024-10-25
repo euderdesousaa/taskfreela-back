19df2fe2a4ecbb7a3e8ebc2a42bea07a43b188b1
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-25 11:15:08 -0300

    Lombok annotation

--- auth-service/src/main/java/tech/engix/auth_service/security/oauth2/user/OAuth2UserInfoFactory.java
+++ auth-service/src/main/java/tech/engix/auth_service/security/oauth2/user/OAuth2UserInfoFactory.java
@@ -1,10 +1,13 @@
 package tech.engix.auth_service.security.oauth2.user;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import tech.engix.auth_service.model.enums.AuthProvider;
 import tech.engix.auth_service.security.oauth2.OAuth2UserInfo;
 
 import java.util.Map;
 
+@NoArgsConstructor(access= AccessLevel.PRIVATE)
 public class OAuth2UserInfoFactory {
 
     public static OAuth2UserInfo getOAuth2UserInfo(String registrationId, Map<String, Object> attributes) throws Exception {
