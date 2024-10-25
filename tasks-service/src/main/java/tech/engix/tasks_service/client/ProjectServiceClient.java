8aa6b32f7a18f8873ad8cf9de9588b79a0d31e36
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-25 11:21:40 -0300

    Import PROJECT-service feignClient

---
+++ tasks-service/src/main/java/tech/engix/tasks_service/client/ProjectServiceClient.java
@@ -0,0 +1,12 @@
+package tech.engix.tasks_service.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import tech.engix.tasks_service.dto.ProjectClientResponse;
+
+@FeignClient(url = "localhost:8083", name = "project-service")
+public interface ProjectServiceClient {
+    @GetMapping("api/v1/project/getProjectId")
+    ProjectClientResponse getClientName(@RequestParam("id") Long id);
+}
