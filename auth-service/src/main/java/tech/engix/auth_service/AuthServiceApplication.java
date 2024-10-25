4822f398250c45223030300887df2969c658e571
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-25 11:16:33 -0300

    FeignClients Annotation

--- auth-service/src/main/java/tech/engix/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/tech/engix/auth_service/AuthServiceApplication.java
@@ -3,10 +3,12 @@ package tech.engix.auth_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import tech.engix.auth_service.config.AppProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(AppProperties.class)
+@EnableFeignClients
 public class AuthServiceApplication {
 
     public static void main(String[] args) {
