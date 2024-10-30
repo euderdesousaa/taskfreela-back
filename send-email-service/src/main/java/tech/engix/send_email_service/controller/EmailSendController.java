785dae82e4112c0d7c8339021e95096cd45921ac
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-30 15:56:24 -0300

    Kafka adaptation

--- send-email-service/src/main/java/tech/engix/send_email_service/controller/EmailSendController.java
+++ send-email-service/src/main/java/tech/engix/send_email_service/controller/EmailSendController.java
@@ -1,33 +1,35 @@
 package tech.engix.send_email_service.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import tech.engix.send_email_service.dto.ResetPasswordMessage;
 import tech.engix.send_email_service.service.EmailSendService;
 
 import java.io.UnsupportedEncodingException;
 
-@RestController
-@RequestMapping("/api/v1/email")
+@Service
 @RequiredArgsConstructor
 public class EmailSendController {
 
     private final EmailSendService service;
 
-    @PostMapping("/send-reset-email")
     @KafkaListener(topics = "auth-reset", groupId = "auth-group")
-    public ResponseEntity<Void> sendResetPassword(@RequestParam("recipientEmail") String recipientEmail,
-                                                  @RequestParam String link) throws MessagingException, UnsupportedEncodingException {
+    public void sendResetPassword(String msgJson) throws MessagingException, UnsupportedEncodingException, JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ResetPasswordMessage message = objectMapper.readValue(msgJson, ResetPasswordMessage.class);
+
+        String recipientEmail = message.getEmail();
+        String link = message.getResetPasswordLink();
+
         service.sendResetPassword(recipientEmail, link);
-        return ResponseEntity.ok().build();
     }
 
-    @PostMapping("/send-welcome-email")
     @KafkaListener(topics = "auth-welcome", groupId = "auth-group")
     public ResponseEntity<Void> sendWelcome(@RequestParam("recipientEmail") String recipientEmail)
             throws MessagingException, UnsupportedEncodingException {
