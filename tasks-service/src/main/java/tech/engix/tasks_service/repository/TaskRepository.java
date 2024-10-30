6fc8b27f9978052764f40aa2d2628308f5bda150
euder-de-sousa <euderdesousaaaa@gmail.com>
2024-10-30 15:56:52 -0300

    new functions

--- tasks-service/src/main/java/tech/engix/tasks_service/repository/TaskRepository.java
+++ tasks-service/src/main/java/tech/engix/tasks_service/repository/TaskRepository.java
@@ -3,6 +3,7 @@ package tech.engix.tasks_service.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import tech.engix.tasks_service.model.Tasks;
+import tech.engix.tasks_service.model.enums.Status;
 
 import java.util.List;
 
@@ -12,4 +13,8 @@ public interface TaskRepository extends MongoRepository<Tasks, String> {
     List<Tasks> findByProjectId(Long projectId);
     boolean existsByProjectIdAndUserId(Long projectId, Long userId);
     boolean existsByIdAndUserId(String id, Long userId);
+
+    long countByProjectId(Long projectId);
+
+    long countByProjectIdAndStatus(Long projectId, Status status);
 }
